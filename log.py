6c7cce449f04045549e46c77b073f5acbf2f36cd
Norman Lorrain <normanlorrain@gmail.com>
2018-04-13 17:08:36 -0600

    cleanup.

--- log.py
+++ log.py
@@ -4,19 +4,21 @@ LONGFORMAT='%(levelname)8s: ''%(filename)-20s: ''%(lineno)4d:\t''%(message)s'
 SHORTFORMAT='%(levelname)-8s: %(message)s'
 
 # Root logger gets everything.  Handlers defined below will filter it out...
-getLogger('').setLevel(DEBUG)
+ROOTLOGGER = getLogger('')
+ROOTLOGGER.setLevel(DEBUG)
 
-# set up logging to file
+# define a Handler to sys.stderr (on by default)
+console = StreamHandler()
+console.setLevel(DEBUG)
+console.setFormatter(Formatter(SHORTFORMAT))
+ROOTLOGGER.addHandler(console)
+
+# Set up logging to file (optional).
 def toFile( filename ):
     filehandler = FileHandler(filename,mode='w')
     filehandler.setLevel(INFO)
     filehandler.setFormatter(Formatter(LONGFORMAT))
-    getLogger('').addHandler(filehandler) 
+    ROOTLOGGER.addHandler(filehandler) 
     info('logging initialized')
 
 
-# define a Handler to sys.stderr
-console = StreamHandler()
-console.setLevel(DEBUG)
-console.setFormatter(Formatter(SHORTFORMAT))
-getLogger('').addHandler(console)
